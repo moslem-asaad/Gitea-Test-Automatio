f18ae7588f27cdf92c92cb51830943cc1dc97b33
moslem-asaad <moslem@post.bgu.ac.il>
2025-01-16 15:01:11 +0200

    modify api tests to run on actions

--- src/test/java/apiTests/GiteaApiTest.java
+++ src/test/java/apiTests/GiteaApiTest.java
@@ -19,7 +19,9 @@ public class GiteaApiTest {
 
     @BeforeAll
     public static void setup() {
-        RestAssured.baseURI = "http://localhost:3000/api/v1";
+        //RestAssured.baseURI = "http://localhost:3000/api/v1";
+        RestAssured.baseURI = "https://8591-5-29-126-14.ngrok-free.app/api/v1";
+
         RestAssured.port = 3000;
         EnvLoader.loadEnv(".env");
         apiToken =  EnvLoader.getEnv("GITEA_API_TOKEN");
